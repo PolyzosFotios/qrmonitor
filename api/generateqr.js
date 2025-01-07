0f55b23f18a274a81ed983fb4c514b12cb8e874a
PolyzosFotios <polyzosfotios@gmail.com>
2025-01-07 21:45:52 +0200

    Update v.02.02

--- api/generateqr.js
+++ api/generateqr.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const id_public = generatePrivateID();
     const id_private = generatePrivateID();
 
-    let totalUrl = `${req.headers.origin}/api/red/${id_public}`;
+    let totalUrl = `${req.headers.origin}/api/generateqr/${id_public}`;
 
     const qrCodeUrl = await qr.toDataURL(totalUrl);
 
@@ -16,10 +16,59 @@ export default async function handler(req, res) {
 
     await dbNewAdd(id_public, id_private, url);
 
-    res.status(200).json({ id_private, qrCodeUrl, qrCodeRedirectUrl: totalUrl });
+    res
+      .status(200)
+      .json({ id_private, qrCodeUrl, qrCodeRedirectUrl: totalUrl });
+  } else if (req.method === "GET") {
+    const { id } = req.query;
+
+    const { data, error } = await supabase
+      .from("generqrcodes")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.error("Error fetching QR code:", error);
+      return res.status(404).json({ error: "QR Code not found" });
+    }
+
+    const updatedScans = updateScan(data.scans);
+
+    const { error: updateError } = await supabase
+      .from("generqrcodes")
+      .update({ scans: updatedScans })
+      .eq("id", id);
+
+    if (updateError) {
+      console.error("Error updating scans:", updateError);
+      return res.status(500).json({ error: "Failed to update scans" });
+    }
+
+    res.redirect(data.url);
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
+}
+
+function updateScan(scans) {
+  const d = new Date();
+  const year = d.getFullYear();
+  const month = d.getMonth();
+  scans = scans.slice(0, -1); // Αφαίρεση τελευταίου κόμματος
+  const scanEntries = scans.split(",");
+  let updatedScans = "";
+
+  const lastEntry = scanEntries[scanEntries.length - 1]?.split(":");
+  if (lastEntry && lastEntry[0] === `${year}.${month}`) {
+    const count = parseInt(lastEntry[1]) + 1;
+    scanEntries[scanEntries.length - 1] = `${year}.${month}:${count}`;
+  } else {
+    scanEntries.push(`${year}.${month}:1`);
+  }
+
+  updatedScans = scanEntries.join(",") + ",";
+  return updatedScans;
 }
 
 function generatePrivateID() {
@@ -27,9 +76,7 @@ function generatePrivateID() {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   for (let i = 0; i < 18; i++) {
-    result += characters.charAt(
-      Math.floor(Math.random() * characters.length)
-    );
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 }
